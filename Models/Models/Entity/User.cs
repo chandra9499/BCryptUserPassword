66f52769154a0cd77fe947b6de275ffda6a30981
chandra9499 <chandrashekar.6569@gmail.com>
2024-07-16 15:13:17 +0530

    Add project files.

---
+++ Models/Models/Entity/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Models.Entity
+{
+    public class User 
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email format")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+    }
+}
